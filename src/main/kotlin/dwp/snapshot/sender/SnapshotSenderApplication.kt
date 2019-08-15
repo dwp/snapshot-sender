ae69789ebbfaffb51147058e88a591bbcb00aba6
Daniel Chicot <dan.chicot@gmail.com>
2019-08-15 14:31:01 +0100

    sync

--- src/main/kotlin/dwp/snapshot/sender/SnapshotSenderApplication.kt
+++ src/main/kotlin/dwp/snapshot/sender/SnapshotSenderApplication.kt
@@ -1,9 +1,10 @@
 package dwp.snapshot.sender
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication (exclude = [DataSourceAutoConfiguration::class])
 class SnapshotSenderApplication
 
 fun main(args: Array<String>) {
