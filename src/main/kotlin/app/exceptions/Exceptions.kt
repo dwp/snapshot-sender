028ed4000ccc8ea897728c89ff8f685720457183
Daniel Chicot <dan.chicot@gmail.com>
2019-08-21 16:17:59 +0100

    Committed the unit tests.

--- src/main/kotlin/app/exceptions/Exceptions.kt
+++ src/main/kotlin/app/exceptions/Exceptions.kt
@@ -5,3 +5,5 @@ class DataKeyDecryptionException(message: String): Exception(message)
 class DataKeyServiceUnavailableException(message: String): Exception(message)
 
 class WriterException(message: String): Exception(message)
+
+class MetadataException(message: String): Exception(message)
