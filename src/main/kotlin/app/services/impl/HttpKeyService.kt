75c649de3b0267fe6ed9a9c9549f1d720518d649
mark matthews <mark.matthews1@dwp.gov.uk>
2019-10-09 14:15:06 +0100

    Add exponential retry policy and tests

--- src/main/kotlin/app/services/impl/HttpKeyService.kt
+++ src/main/kotlin/app/services/impl/HttpKeyService.kt
@@ -1,63 +1,74 @@
 package app.services.impl
 
-import app.configuration.HttpClientProvider
-import app.domain.DataKeyResult
-import app.exceptions.DataKeyDecryptionException
-import app.exceptions.DataKeyServiceUnavailableException
-import app.services.KeyService
-import com.google.gson.Gson
-import org.apache.http.client.methods.HttpPost
-import org.apache.http.entity.ContentType
-import org.apache.http.entity.StringEntity
-import org.apache.http.util.EntityUtils
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.URLEncoder
+import app.configuration.*
+import app.domain.*
+import app.exceptions.*
+import app.services.*
+import com.google.gson.*
+import org.apache.http.client.methods.*
+import org.apache.http.entity.*
+import org.apache.http.util.*
+import org.slf4j.*
+import org.springframework.beans.factory.annotation.*
+import org.springframework.retry.annotation.*
+import org.springframework.stereotype.*
+import java.io.*
+import java.net.*
 
 @Service
 class HttpKeyService(private val httpClientProvider: HttpClientProvider) : KeyService {
 
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger(HttpKeyService::class.toString())
+        const val maxAttempts = 10
+        const val initialBackoffMillis = 1000L
+        const val backoffMultiplier = 2.0
+    }
+
+    @Override
+    @Retryable(value = [DataKeyServiceUnavailableException::class],
+        maxAttempts = maxAttempts,
+        backoff = Backoff(delay = initialBackoffMillis, multiplier = backoffMultiplier))
     override fun decryptKey(encryptionKeyId: String, encryptedKey: String): String {
         logger.info("Decrypting encryptedKey: '$encryptedKey', keyEncryptionKeyId: '$encryptionKeyId'.")
         val cacheKey = "$encryptedKey/$encryptionKeyId"
-        return if (decryptedKeyCache.containsKey(cacheKey)) {
-            decryptedKeyCache[cacheKey]!!
-        }
-        else {
-            httpClientProvider.client().use { client ->
-                val url = """$dataKeyServiceUrl/datakey/actions/decrypt?keyId=${URLEncoder.encode(encryptionKeyId, "US-ASCII")}"""
-                logger.info("url: '$url'.")
-                val httpPost = HttpPost(url)
-                httpPost.entity = StringEntity(encryptedKey, ContentType.TEXT_PLAIN)
-                client.execute(httpPost).use { response ->
-                    return when {
-                        response.statusLine.statusCode == 200 -> {
-                            val entity = response.entity
-                            val text = BufferedReader(InputStreamReader(response.entity.content)).use(BufferedReader::readText)
-                            EntityUtils.consume(entity)
-                            val dataKeyResult = Gson().fromJson(text, DataKeyResult::class.java)
-                            decryptedKeyCache[cacheKey] = dataKeyResult.plaintextDataKey
-                            dataKeyResult.plaintextDataKey
+        try {
+            return if (decryptedKeyCache.containsKey(cacheKey)) {
+                decryptedKeyCache[cacheKey]!!
+            } else {
+                httpClientProvider.client().use { client ->
+                    val url = """$dataKeyServiceUrl/datakey/actions/decrypt?keyId=${URLEncoder.encode(encryptionKeyId, "US-ASCII")}"""
+                    logger.info("url: '$url'.")
+                    val httpPost = HttpPost(url)
+                    httpPost.entity = StringEntity(encryptedKey, ContentType.TEXT_PLAIN)
+                    client.execute(httpPost).use { response ->
+                        return when {
+                            response.statusLine.statusCode == 200 -> {
+                                val entity = response.entity
+                                val text = BufferedReader(InputStreamReader(response.entity.content)).use(BufferedReader::readText)
+                                EntityUtils.consume(entity)
+                                val dataKeyResult = Gson().fromJson(text, DataKeyResult::class.java)
+                                decryptedKeyCache[cacheKey] = dataKeyResult.plaintextDataKey
+                                dataKeyResult.plaintextDataKey
+                            }
+                            response.statusLine.statusCode == 400 ->
+                                throw DataKeyDecryptionException(
+                                    "Decrypting encryptedKey: '$encryptedKey' with keyEncryptionKeyId: '$encryptionKeyId' data key service returned status code '${response.statusLine.statusCode}'".trimMargin())
+                            else ->
+                                throw DataKeyServiceUnavailableException(
+                                    "Decrypting encryptedKey: '$encryptedKey' with keyEncryptionKeyId: '$encryptionKeyId' data key service returned status code '${response.statusLine.statusCode}'".trimMargin())
                         }
-                        response.statusLine.statusCode == 400 ->
-                            throw DataKeyDecryptionException(
-                                    """Decrypting encryptedKey: '$encryptedKey' with 
-                            |keyEncryptionKeyId: '$encryptionKeyId'
-                            |data key service returned status code '${response.statusLine.statusCode}'""".trimMargin())
-                        else ->
-                            throw DataKeyServiceUnavailableException(
-                                    """Decrypting encryptedKey: '$encryptedKey' with 
-                            |keyEncryptionKeyId: '$encryptionKeyId'
-                            |data key service returned status code '${response.statusLine.statusCode}'""".trimMargin())
+
                     }
 
                 }
-
             }
+        } catch (ex: DataKeyDecryptionException) {
+            throw ex
+        } catch (ex: DataKeyServiceUnavailableException) {
+            throw ex
+        } catch (ex: Exception) {
+            throw DataKeyServiceUnavailableException("Error contacting data key service: ${ex.javaClass.name}: $ex.message")
         }
     }
 
@@ -69,9 +80,4 @@ class HttpKeyService(private val httpClientProvider: HttpClientProvider) : KeySe
 
     @Value("\${data.key.service.url}")
     private lateinit var dataKeyServiceUrl: String
-
-    companion object {
-        val logger: Logger = LoggerFactory.getLogger(HttpKeyService::class.toString())
-    }
-
 }
