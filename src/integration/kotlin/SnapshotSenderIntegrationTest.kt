1f39412cd4a73732e849c42c53f750229f615d5c
markmatthews <markmatthews@arkmEnterprises.co.uk>
2019-10-18 13:18:48 +0100

    Prototype integration test

--- src/integration/kotlin/SnapshotSenderIntegrationTest.kt
+++ src/integration/kotlin/SnapshotSenderIntegrationTest.kt
@@ -12,15 +12,17 @@ class SnapshotSenderIntegrationTest : StringSpec() {
         val logger: Logger = LoggerFactory.getLogger(SnapshotSenderIntegrationTest::class.toString())
     }
 
+    val s3Bucket = System.getenv("s3.bucket") ?: "demobucket"
     val s3PrefixFolder = System.getenv("s3.prefix.folder") ?: "test/output/"
     val s3HtmeRootFolder = System.getenv("s3.htme.root.folder") ?: "test"
     val s3StatusFolder = System.getenv("s3.status.folder") ?: "status"
-    val s3ServiceEndpoint = System.getenv("s3.service.endpoint") ?: "http://s3-dummy:4572"
+    val s3ServiceEndpoint = System.getenv("s3.service.endpoint") ?: "http://localhost:4572"
     val nifiRootFolder = System.getenv("nifi.root.folder") ?: "/data/output"
     val nifiTimestamp = System.getenv("nifi.timestamp") ?: "10"
     val nifiFileNames = System.getenv("nifi.file.names")
         ?: "/data/output/db.core.addressDeclaration/db.core.addressDeclaration-000001.txt.bz2"
     val nifiLinecounts = System.getenv("nifi.file.linecounts") ?: "7"
+    var bucketUri = "$s3ServiceEndpoint/$s3Bucket"
 
     init {
         "Verify vars" {
@@ -35,7 +37,7 @@ class SnapshotSenderIntegrationTest : StringSpec() {
         }
 
         "Verify for every source collection a finished file was written to s3" {
-            logger.info("Hello Mum 1")
+            logger.info("Hello Mum 1: $bucketUri")
 
             //s3 in:  test/output/db.core.addressDeclaration-000001.txt.bz2.enc
             //s3 out: test/status/db.core.addressDeclaration-000001.txt.bz2.enc.finished
@@ -43,7 +45,7 @@ class SnapshotSenderIntegrationTest : StringSpec() {
             // fetch http://s3-dummy:4572/demobucket/status/output/db.core.addressDeclaration-000001.txt.bz2.enc.finished
             // fetch http://s3-dummy:4572/demobucket/status/output/db.core.addressDeclaration-000001.txt.bz2.enc.finished
             //content = "Finished test/output/db.core.addressDeclaration-000001.txt.bz2.enc"
-            val results = Request.Get("http://s3-dummy:4572/demobucket")
+            val results = Request.Get(bucketUri)
                 .connectTimeout(100000)
                 .socketTimeout(100000)
                 .execute().returnContent().asString()
