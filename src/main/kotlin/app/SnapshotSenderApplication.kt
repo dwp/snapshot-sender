45b268af5714dc17425b2341e6aef59e9d09dfea
Daniel Chicot <dan.chicot@gmail.com>
2020-04-05 11:46:53 +0100

    Add sqs_message_id to all log messages.

--- src/main/kotlin/app/SnapshotSenderApplication.kt
+++ src/main/kotlin/app/SnapshotSenderApplication.kt
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
+import uk.gov.dwp.dataworks.logging.LogFields
 import kotlin.system.exitProcess
 
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
@@ -12,5 +13,6 @@ import kotlin.system.exitProcess
 class SnapshotSenderApplication
 
 fun main(args: Array<String>) {
+    LogFields.put("SQS_MESSAGE_ID", "sqs_message_id", "NOT_SET")
     exitProcess(SpringApplication.exit(runApplication<SnapshotSenderApplication>(*args)))
 }
