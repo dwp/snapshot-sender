22825fc1af45efc4896743a9e6af58db44bdd64f
Daniel Chicot <dan.chicot@gmail.com>
2021-01-22 16:26:32 +0000

    sync

--- src/integration/kotlin/SnapshotSenderIntegrationTest.kt
+++ src/integration/kotlin/SnapshotSenderIntegrationTest.kt
@@ -7,159 +7,49 @@ import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.model.AttributeValue
 import com.amazonaws.services.dynamodbv2.model.GetItemRequest
-import com.beust.klaxon.Parser
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.S3ObjectSummary
 import io.kotlintest.fail
-import io.kotlintest.matchers.collections.shouldContain
-import io.kotlintest.matchers.collections.shouldContainAll
-import io.kotlintest.matchers.numerics.shouldBeGreaterThan
-import io.kotlintest.matchers.numerics.shouldBeGreaterThanOrEqual
-import io.kotlintest.matchers.string.shouldNotBeEmpty
+import io.kotlintest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotlintest.matchers.types.shouldNotBeNull
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import org.apache.commons.compress.compressors.CompressorStreamFactory
-import org.apache.http.client.fluent.Request
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.w3c.dom.NodeList
-import org.xml.sax.InputSource
 import java.io.*
-import javax.xml.parsers.DocumentBuilderFactory
 
+@Suppress("BlockingMethodInNonBlockingContext")
 class SnapshotSenderIntegrationTest : StringSpec() {
 
-    companion object {
-        val logger: Logger = LoggerFactory.getLogger(SnapshotSenderIntegrationTest::class.toString())
-    }
-
-    private val s3Bucket = System.getenv("S3_BUCKET") ?: "demobucket"
-    private val s3PrefixFolder = System.getenv("S3_PREFIX_FOLDER") ?: "test/output/"
-    private val s3HtmeRootFolder = System.getenv("S3_HTME_ROOT_FOLDER") ?: "test"
-    private val s3StatusFolder = System.getenv("S3_STATUS_FOLDER") ?: "status"
-    private val s3ServiceEndpoint = System.getenv("S3_SERVICE_ENDPOINT") ?: "http://localhost:4572"
-    private val nifiRootFolder = System.getenv("NIFI_ROOT_FOLDER") ?: "/data/output"
-
-    //matched triplets of file name, timestamp and line count
-    private val nifiFileNamesCSV = System.getenv("NIFI_FILE_NAMES_CSV")
-        ?: "db.core.addressDeclaration-045-050-000001.json.gz,db.quartz.claimantEvent-045-050-000001.json.gz"
-    private val nifiLineCountsCSV = System.getenv("NIFI_LINE_COUNTS_CSV") ?: "7,1"
-    private val nifiTimestampsCSV = System.getenv("NIFI_TIME_STAMPS_CSV") ?: "10,1"
-
-    private val bucketUri = "$s3ServiceEndpoint/$s3Bucket"
-    private val s3SourceExporterFolder = s3PrefixFolder
-    private val s3SenderStatusFolder = s3SourceExporterFolder.replace("$s3HtmeRootFolder/", "$s3StatusFolder/")
-    private val nifiFileNames = nifiFileNamesCSV.split(",")
-    private val nifiLineCounts = nifiLineCountsCSV.split(",")
-    private val nifiTimestamps = nifiTimestampsCSV.split(",")
-
-    private val dbFactory = DocumentBuilderFactory.newInstance()
-    private val dBuilder = dbFactory.newDocumentBuilder()
-    private val jsonParser: Parser = Parser.default()
-
     init {
-
-        "Verify env vars were passed from docker via gradle to here" {
-            s3Bucket.shouldNotBeEmpty()
-            s3PrefixFolder.shouldNotBeEmpty()
-            s3HtmeRootFolder.shouldNotBeEmpty()
-            s3StatusFolder.shouldNotBeEmpty()
-            s3ServiceEndpoint.shouldNotBeEmpty()
-            nifiRootFolder.shouldNotBeEmpty()
-            nifiFileNamesCSV.shouldNotBeEmpty()
-            nifiLineCountsCSV.shouldNotBeEmpty()
-            nifiTimestampsCSV.shouldNotBeEmpty()
-            bucketUri.length.shouldBeGreaterThan(1)
-            s3SourceExporterFolder.shouldNotBeEmpty()
-            s3SenderStatusFolder.shouldNotBeEmpty()
-            nifiFileNames.size.shouldBeGreaterThanOrEqual(1)
-            nifiLineCounts.size.shouldBeGreaterThanOrEqual(1)
-            nifiTimestamps.size.shouldBeGreaterThanOrEqual(1)
-            //matched triplets of file name, timestamp and line count
-            nifiLineCounts.size.shouldBe(nifiFileNames.size)
-            nifiTimestamps.size.shouldBe(nifiFileNames.size)
+        "There should be a finished file for every input file" {
+            val inputs = s3.listObjectsV2(BUCKET, INPUT_FOLDER).objectSummaries.map(S3ObjectSummary::getKey).map(::File).map(File::getName)
+            val successes = s3.listObjectsV2(BUCKET, STATUS_FILE_FOLDER).objectSummaries.map(S3ObjectSummary::getKey)
+                .map(::File).map(File::getName).map { it.replace(Regex(".finished$"), "") }
+            inputs shouldContainExactlyInAnyOrder listOf("db.core.addressDeclaration-045-050-000001.txt.gz.enc",
+                "db.quartz.claimantEvent-045-050-000001.txt.gz.enc")
+            successes shouldContainExactlyInAnyOrder inputs
         }
 
-        "Verify for every source collection a finished file was written to s3" {
-            logger.info("Checking $bucketUri ...")
+        "There should be a nifi file for every input file" {
+            val inputs = s3.listObjectsV2(BUCKET, INPUT_FOLDER).objectSummaries.map(S3ObjectSummary::getKey)
+                .map(::File).map(File::getName).map { it.replace(Regex("""\.txt\.gz\.enc$"""), "")}
 
-            //s3 in:  test/output/db.core.addressDeclaration-000001.json.gz.enc
-            //s3 out: test/status/db.core.addressDeclaration-000001.json.gz.enc.finished
-
-            // fetch http://aws:4572/demobucket/status/output/db.core.addressDeclaration-000001.json.gz.enc.finished
-            // ...should have content = "Finished test/output/db.core.addressDeclaration-000001.json.gz.enc"
-
-            val bucketResultsXml = getS3Content(bucketUri)
-            val fileKeys = getXmlNodesByTagName("Key", bucketResultsXml)
-            val allKeys = getFileKeys(fileKeys)
-            val exporterKeys = allKeys.filter { it.startsWith(s3SourceExporterFolder) }
-            logger.info("exporterKeys: $exporterKeys")
-            val statusKeys = allKeys.filter { it.startsWith(s3SenderStatusFolder) }
-            logger.info("statusKeys: $statusKeys")
-            exporterKeys.size.shouldBeGreaterThan(0)
-            exporterKeys.size.shouldBe(statusKeys.size)
-
-            val matchedExporterAndStatusKeys = mutableMapOf<String, String>()
-            exporterKeys.forEach { exporterKey ->
-                val withUpdatedFolder = exporterKey.replace(s3SourceExporterFolder, s3SenderStatusFolder)
-                val expectedStatusKey = "$withUpdatedFolder.finished"
-                logger.info("Checking statusKeys contains '$expectedStatusKey'")
-                statusKeys.shouldContain(expectedStatusKey)
-                matchedExporterAndStatusKeys[exporterKey] = expectedStatusKey
-            }
-
-            matchedExporterAndStatusKeys.forEach { pair ->
-                val fullPath = "$bucketUri/${pair.value}"
-                logger.info("Loading status file '$fullPath'")
-                val keyResult = getS3Content(fullPath)
-                logger.info("$keyResult file contains text '$keyResult'")
-                logger.info("Checking file '${pair.value}' contains '${pair.key}'")
-                keyResult.shouldBe("Finished ${pair.key}")
-            }
-        }
-
-        "Verify for every source collection an output file was sent to nifi as gz" {
-            logger.info("Check collections vs nifi")
-            //s3 in:    test/output/db.core.addressDeclaration-000001.json.gz.enc
-            //nifi out: /data/output/db.core.addressDeclaration/db.core.addressDeclaration-000001.json.gz
-
-            val bucketResultsXml = getS3Content(bucketUri)
-            val fileKeys = getXmlNodesByTagName("Key", bucketResultsXml)
-            val allKeys = getFileKeys(fileKeys)
-            val exporterKeys = allKeys.filter { it.startsWith(s3SourceExporterFolder) }
-            logger.info("exporterKeys: $exporterKeys")
-
-            val exporterKeysToMatchNifi = exporterKeys
-                .asSequence()
-                .map { it.replace(s3SourceExporterFolder, "") }
-                .map { it.replace(".enc", "") }
-                .map { it.replace(Regex("^/"), "") }
-                .map {
-                    Pair(File(it).name
-                        .replace(Regex("-.*$"), "")
-                        .replace(Regex("^.*/"), ""), it)
-                }
-                .map { (collection, filename) ->
-                    "$nifiRootFolder/$collection/$filename"
-                }
-                .map { path ->
-                    path.replace(Regex("""\.txt\.gz$"""), ".json.gz")
-                }
+            val outputs = File(NIFI_OUTPUT_FOLDER).walkTopDown()
+                .filter(File::isFile).map(File::getName)
+                .filter { it.endsWith(".json.gz") }
+                .map { it.replace(Regex("""\.json\.gz$"""), "") }
                 .toList()
-
-            logger.info("exporterKeysToMatchNifi: $exporterKeysToMatchNifi")
-
-            val actualNifiFiles = File(nifiRootFolder).walkTopDown()
-                .map { it.absolutePath }
-                .filter { it.contains("db.") && it.contains(".json.gz") }
-                .toList()
-            logger.info("actualNifiFiles: $actualNifiFiles")
-
-            actualNifiFiles.shouldContainAll(exporterKeysToMatchNifi)
-            actualNifiFiles.size.shouldBe(exporterKeysToMatchNifi.size)
+            inputs shouldContainExactlyInAnyOrder listOf("db.core.addressDeclaration-045-050-000001",
+                "db.quartz.claimantEvent-045-050-000001")
+            outputs shouldContainExactlyInAnyOrder inputs
         }
 
         "Export status is sent" {
             val dynamoDB = AmazonDynamoDBClientBuilder.standard()
-                    .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://aws:4566/",
+                    .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration("http://aws:4566",
                             "eu-west-2"))
                     .withClientConfiguration(ClientConfiguration().withProtocol(Protocol.HTTP))
                     .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials("access-key", "secret-key")))
@@ -178,18 +68,18 @@ class SnapshotSenderIntegrationTest : StringSpec() {
             val status = item["CollectionStatus"]
             val filesExported = item["FilesExported"]
             val filesSent = item["FilesSent"]
-            status?.s shouldBe "Sent"
-            filesExported?.n shouldBe "2"
-            filesSent?.n shouldBe "2"
+            status.shouldNotBeNull()
+            status.s shouldBe "Sent"
+            filesExported.shouldNotBeNull()
+            filesExported.n shouldBe "2"
+            filesSent.shouldNotBeNull()
+            filesSent.n shouldBe "2"
         }
 
         "Verify nifi output files have a valid json per line at expected timestamp with specified line count" {
             logger.info("Check nifi outputs")
-            // Verify "-file /data/output/db.core.addressDeclaration/db.core.addressDeclaration-000001.json.gz \
-            //              -linecount 7"
-            //              -timestamp 10 \
 
-            val actualNifiFiles = File(nifiRootFolder).walkTopDown()
+            val actualNifiFiles = File(NIFI_OUTPUT_FOLDER).walkTopDown()
                 .map { it.absolutePath }
                 .filter { it.contains("db.") && it.contains(".json.gz") }
                 .toList()
@@ -198,7 +88,7 @@ class SnapshotSenderIntegrationTest : StringSpec() {
             for (index in nifiFileNames.indices) {
                 val expectedFile = nifiFileNames[index]
                 val collection = deriveCollection("/$expectedFile")
-                val fullPath = "$nifiRootFolder/$collection/$expectedFile"
+                val fullPath = "$NIFI_OUTPUT_FOLDER/$collection/$expectedFile"
                 val expectedLineCount = nifiLineCounts[index].toInt()
                 val expectedTimestamp = nifiTimestamps[index].toLong()
 
@@ -213,15 +103,16 @@ class SnapshotSenderIntegrationTest : StringSpec() {
                 var linesDone = 0
                 do {
                     val line = dataReader.readLine()
-                    println(line)
-                    if (line != null) {
-                        linesDone++
-                    }
-                    else if (linesDone == expectedLineCount) {
-                        logger.info("Skipping blank line at EOF as should be end of file: have processed $linesDone/$expectedLineCount in $expectedFile")
-                    }
-                    else {
-                        fail("Did not expect blank line before EOF: have only processed $linesDone/$expectedLineCount in $expectedFile")
+                    when {
+                        line != null -> {
+                            linesDone++
+                        }
+                        linesDone == expectedLineCount -> {
+                            logger.info("Skipping blank line at EOF as should be end of file: have processed $linesDone/$expectedLineCount in $expectedFile")
+                        }
+                        else -> {
+                            fail("Did not expect blank line before EOF: have only processed $linesDone/$expectedLineCount in $expectedFile")
+                        }
                     }
                 }
                 while (line != null)
@@ -233,37 +124,6 @@ class SnapshotSenderIntegrationTest : StringSpec() {
 
             actualNifiFiles.size.shouldBe(nifiFileNames.size)
         }
-    }
-
-    private fun getFileKeys(fileKeys: NodeList): MutableList<String> {
-        val allKeys = mutableListOf<String>()
-        for (index in 0 until fileKeys.length) {
-            val key = fileKeys.item(index)
-            val keyText = key.textContent
-            logger.info("keyText: $keyText")
-            allKeys.add(keyText)
-        }
-        return allKeys
-    }
-
-    private fun getS3Content(s3FullPath: String): String {
-        val results = Request.Get(s3FullPath)
-            .connectTimeout(1000)
-            .socketTimeout(1000)
-            .execute().returnContent().asString()
-        logger.info("S3 results for '$s3FullPath':\n$results")
-        if (results == null) {
-            fail("No s3 file found for $s3FullPath")
-        }
-        return results
-    }
-
-    private fun getXmlNodesByTagName(keyName: String, sourceXmlString: String): NodeList {
-        val xmlInput = InputSource(StringReader(sourceXmlString))
-        val doc = dBuilder.parse(xmlInput)
-        val keys = doc.getElementsByTagName(keyName) ?: fail("No elements found for '$keyName' in given xml")
-        logger.info("Found ${keys.length} keys with tag name '$keyName'")
-        return keys
     }
 
     private fun deriveCollection(fileName: String): String {
@@ -281,5 +141,34 @@ class SnapshotSenderIntegrationTest : StringSpec() {
         val fullCollection = fileName.substring((lastSlashIndex + 1) until (lastDashIndex)).replace(Regex("""-\d{3}-\d{3}$"""), "")
         logger.info("Derived collection: '${fullCollection}' from fileName of '$fileName'")
         return fullCollection
+    }
+
+    companion object {
+        private val nifiFileNames = listOf("db.core.addressDeclaration-045-050-000001.json.gz","db.quartz.claimantEvent-045-050-000001.json.gz") //nifiFileNamesCSV.split(",")
+        private val nifiLineCounts = listOf("7", "1")
+        private val nifiTimestamps = listOf("10", "1")
+
+        private const val INPUT_FOLDER = "test/output"
+        private const val BUCKET = "demobucket"
+        private const val NIFI_OUTPUT_FOLDER = "/data/output"
+
+        private const val S3_SERVICE_ENDPOINT = "http://aws:4566"
+        private const val STATUS_FILE_FOLDER = "status/output"
+        private const val SIGNING_REGION = "eu-west-2"
+        private const val ACCESS_KEY = "accessKey"
+        private const val SECRET_KEY = "secretKey"
+
+        private val s3: AmazonS3 by lazy {
+            with (AmazonS3ClientBuilder.standard()) {
+                withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(S3_SERVICE_ENDPOINT, SIGNING_REGION))
+                withClientConfiguration(ClientConfiguration().withProtocol(Protocol.HTTP))
+                withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+                withPathStyleAccessEnabled(true)
+                disableChunkedEncoding()
+                build()
+            }
+        }
+
+        private val logger: Logger = LoggerFactory.getLogger(SnapshotSenderIntegrationTest::class.toString())
     }
 }
