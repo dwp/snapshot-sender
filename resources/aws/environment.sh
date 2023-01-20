66d867309b3dde07de0f61f2f41b614e61ade9aa
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-01-20 12:58:55 +0000

    switching to an ARN instaed of URL

--- resources/aws/environment.sh
+++ resources/aws/environment.sh
@@ -62,7 +62,7 @@ sqs_list_queues() {
 
 subscribe_sns_to_sqs() {    
     aws_local sns subscribe --region us-east-1 --topic-arn "arn:aws:sns:us-east-1:000000000000:monitoring-topic" \
-     --protocol "sqs" --notification-endpoint "http://aws:4566/000000000000/monitoring-queue.fifo" --debug
+     --protocol "sqs" --notification-endpoint "arn:aws:sqs:us-east-1:000000000000:monitoring-queue.fifo"
 }
 
 add_status_item() {
