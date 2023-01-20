fd916398caa1ed2fd547d9b8185c4c4d0da8c6d9
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-01-20 10:33:09 +0000

    Amending topic back to non fifo

--- resources/aws/environment.sh
+++ resources/aws/environment.sh
@@ -49,7 +49,7 @@ create_uc_ecc_table() {
 }
 
 create_sns_monitoring_topic() {
-    aws_local sns create-topic --region eu-west-2 --name "monitoring-topic.fifo" --attributes "FifoTopic=true"
+    aws_local sns create-topic --region eu-west-2 --name "monitoring-topic"
 }
 
 create_sqs_monitoring_queue() {
@@ -57,7 +57,7 @@ create_sqs_monitoring_queue() {
 }
 
 subscribe_sns_to_sqs() {
-    aws_local sns subscribe --region eu-west-2 --topic-arn "arn:aws:sns:eu-west-2:000000000000:monitoring-topic.fifo" \
+    aws_local sns subscribe --region eu-west-2 --topic-arn "arn:aws:sns:eu-west-2:000000000000:monitoring-topic" \
      --protocol "sqs" --notification-endpoint "http://aws:4566/000000000000/monitoring-queue.fifo"
 }
 
