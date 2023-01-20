1268db7d38fb5f09e391a94fc36c44498a2b91a3
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2023-01-20 10:50:16 +0000

    Reverting to localhost

--- resources/aws/environment.sh
+++ resources/aws/environment.sh
@@ -58,7 +58,7 @@ create_sqs_monitoring_queue() {
 
 subscribe_sns_to_sqs() {
     aws_local sns subscribe --region eu-west-2 --topic-arn "arn:aws:sns:eu-west-2:000000000000:monitoring-topic" \
-     --protocol "sqs" --notification-endpoint "http://aws:4566/000000000000/monitoring-queue.fifo"
+     --protocol "sqs" --notification-endpoint "http://localhost:4566/000000000000/monitoring-queue.fifo"
 }
 
 add_status_item() {
